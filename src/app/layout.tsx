cfc031e239e97682fe99b5b295eb644add358671
Pranav Bhat <pranav2bhat@gmail.com>
2026-06-26 00:50:15 -0400

    favicon change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,17 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Pranav Bhat",
   description: "Pranav Bhat's Portfolio",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: {
+      url: "/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+  },
 };
 
 export default function RootLayout({
